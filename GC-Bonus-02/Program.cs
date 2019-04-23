145fb306954c46eb9209a5d028683661e53671b9
StevenVallarsa <orionsjv@gmail.com>
2019-04-23 15:55:26 -0400

    Prettied up code

--- GC-Bonus-02/Program.cs
+++ GC-Bonus-02/Program.cs
@@ -6,22 +6,23 @@ namespace GC_Bonus_02
     {
         static void Main(string[] args)
         {
-            var dateToday = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day);
-
+            // create integers for user to provide year, month and day of birth
             int year, month, day;
 
+            // validate user-inputted data to be integers and to be real
+            // ie. no month 13 or day 65
             while (true)
             {
                 Console.Write("What year were you born? (YYYY) ");
                 year = CheckValidity();
 
-                if (year > 1900 && year < 2010)
+                if (year > 1900 && year < 2011)
                 {
                     break;
                 }
 
-                Console.WriteLine("That doesn't seem right. Please try again"); 
-                Console.WriteLine("with a year between 1901 and 2010.");
+                Console.WriteLine("That can't be right. Please try again"); 
+                Console.WriteLine("with a year between 1901 and 2010.\n");
             } 
 
             while (true)
@@ -34,8 +35,8 @@ namespace GC_Bonus_02
                     break;
                 }
 
-                Console.WriteLine("That doesn't seem right. Please try again");
-                Console.WriteLine("with an integer between 01 and 12.");
+                Console.WriteLine("That's not right. Please try again");
+                Console.WriteLine("with an integer between 01 and 12.\n");
             }
 
             while (true)
@@ -48,37 +49,39 @@ namespace GC_Bonus_02
                     break;
                 }
 
-                Console.WriteLine("That doesn't seem right. Please try again");
+                Console.WriteLine("That can't be right. Please try again");
                 Console.WriteLine("with an integer between 01 and 31.");
 
             }
 
+            // get today as a DATETIME object
+            var dateToday = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day);
 
+            // convert user-entered brithday information into a DATETIME object
             var birthDate = new DateTime(year, month, day);
 
+            // get number of days as TIMESPAN object
             var daysPast =  dateToday - birthDate;
 
+            // convert days into an integer
             int daysLived = Convert.ToInt32(daysPast.TotalDays);
 
            
             Console.WriteLine($"\nOh my! You're lived...\n");
-            Console.WriteLine($"  {daysLived} days");
+            Console.WriteLine($"  {daysLived} days\n");
+            Console.WriteLine("And assuming it's midnight and you were born at midnight...\n");
+
             Console.WriteLine($"  {daysLived * 24} hours");
             Console.WriteLine($"  {daysLived * 24 * 60} minute");
             Console.WriteLine("   or");
             Console.WriteLine($"  {daysLived * 24 * 60 * 60} seconds!");
-            Console.WriteLine("  (assuming it's midnight and you were born at midnight, of course.)");
 
 
+            // check to see if today is user's birthday
             if (month == DateTime.Today.Month && day == DateTime.Today.Day)
             {
                 Console.WriteLine("\nAnd OHMYGOSH! Happy Birthday!");
             }
-
-
-
-
-
 
         }
 
@@ -88,7 +91,7 @@ namespace GC_Bonus_02
         static int CheckValidity()
         {
 
-            // Keep asking for user input until valid number is returned.
+            // Keep asking for user input until integer is returned.
             while (true)
             {
 
@@ -107,26 +110,8 @@ namespace GC_Bonus_02
 
 
             }
+        
         }
-
-
-
-
-            //// try to convert input into intiger, else make year 1967
-            //if(!Int32.TryParse(yearString, out year))
-            //{
-            //    Console.WriteLine("That's not a valid year. Let's just say you were born in 1967, because, reasons!");
-            //    year = 1967;
-            //}
-
-            //// check for obvious wrong years and make it 1967 instead
-            //if (year < 1900 || year > 2009)
-            //{
-            //    Console.WriteLine("Your birthyear seems odd. Let's just say you were born in 1967, becasue, reasons!");
-            //    year = 1967;
-            //}
-
-            //Console.WriteLine($"The year is {year}");
 
     }
 
